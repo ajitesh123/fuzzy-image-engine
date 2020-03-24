1776eceb21a37b7f6658f0d72c0ede92cccc58ae
Ajitesh Abhishek <ajiteshleo@gmail.com>
2020-03-24 15:36:57 -0500

    EBS implemented

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Router, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
@@ -28,6 +28,18 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
+  app.get( "/filteredimage/", async ( req: Request, res: Response ) => {
+    const url = req.query.image_url as string;
+
+    if ( !url ) {
+      return res.status(400)
+                .send(`url is required`);
+    }
+    let filteredpath = await filterImageFromURL(url);
+
+    return res.status(200)
+              .sendFile(filteredpath, () => deleteLocalFiles([filteredpath]));
+  } );
 
   //! END @TODO1
   
